d05a85274760d548928507f7809b70f67e3dad68
AmirAnis <amir.anis.rkl@gmail.com>
2022-04-02 01:15:39 +0530

    setup directory structure

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+const home = function(req, res){
+    res.end('hi');
+}
+
+module.exports.home = home;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const port = 8000;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.static('./assets'));
+app.use('/', require('./routes'));
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is up and running on port: ${port}`);
+});
+
