12655c1420325a884aff0871a5af77cb7d203ece
AmirAnis <amir.anis.rkl@gmail.com>
2022-04-03 19:55:15 +0530

    setup manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,21 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: "User profile"
-    });
+    if(req.cookies.user_id){
+
+        // User.findById runs asynchronously so if else statement is both necessary
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                return res.render('user_profile', {
+                    title: "User profile",
+                    user: user 
+                });
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 };
 
 module.exports.signUp = function(req, res){
@@ -47,5 +59,32 @@ module.exports.create = function(req, res){
 
 // sign in and create session for the user
 module.exports.createSession = function(req, res){
-    // To do later
+    // Find the user
+    User.findOne({
+        email: req.body.email 
+    }, function(err, user){
+        if(err){
+            console.log('Error in finding the user while signing in');
+            return;
+        }
+
+        // handle user found
+        if(user){
+            // handle password mismatch
+
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            console.log(user.id);
+            console.log(user._id);
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }else{
+            // handle user not found
+
+            res.redirect('back');
+        }
+    });
 };
--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,6 @@ router.get('/sign-up', usersController.signUp);
 router.get('/profile', usersController.profile);
 
 router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
 
 module.exports = router;
