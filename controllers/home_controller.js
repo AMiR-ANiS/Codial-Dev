8b0ed9a95d4807e02c74cca89260fdbedefdb50f
AmirAnis <amir.anis.rkl@gmail.com>
2022-04-07 03:31:05 +0530

    show posts and the user who posted it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,15 @@ const Post = require('../models/post');
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 34);
-    Post.find({}, function(err,posts){
+    // Post.find({}, function(err,posts){
+    //     return res.render('home',{
+    //         title: 'Codial | Home',
+    //         posts: posts 
+    //     });
+    // });
+
+    // Populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
         return res.render('home',{
             title: 'Codial | Home',
             posts: posts 
