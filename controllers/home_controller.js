b14b4f512abb7ebac92d29b9fbef5a1854349011
AmirAnis <amir.anis.rkl@gmail.com>
2022-04-06 20:13:56 +0530

    add stylesheets to header, footer and rest view files

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,12 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 34);
-    return res.render('home', {
-        title: "Codeial | Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 34);
+    Post.find({}, function(err,posts){
+        return res.render('home',{
+            title: 'Codial | Home',
+            posts: posts 
+        });
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 router.get('/', homeController.home);
 
