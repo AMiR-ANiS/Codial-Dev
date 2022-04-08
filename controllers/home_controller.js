1da3470ecea43805d75bddc00142fae680cd40eb
AmirAnis <amir.anis.rkl@gmail.com>
2022-04-09 05:17:48 +0530

    convert home controller function to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,35 +1,65 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 34);
-    // Post.find({}, function(err,posts){
-    //     return res.render('home',{
-    //         title: 'Codial | Home',
-    //         posts: posts 
-    //     });
-    // });
+// module.exports.home = function(req, res){
+//     // console.log(req.cookies);
+//     // res.cookie('user_id', 34);
+//     // Post.find({}, function(err,posts){
+//     //     return res.render('home',{
+//     //         title: 'Codial | Home',
+//     //         posts: posts 
+//     //     });
+//     // });
 
-    // Populate the user of each post
-    Post.find({}).populate('user')
-    .populate({
-        path: 'comments',
-        populate:{
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({}, function(err, users){
-            if(err){
-                console.log('cannot fetch users from the db');
-                return;
+//     // Populate the user of each post
+//     Post.find({}).populate('user')
+//     .populate({
+//         path: 'comments',
+//         populate:{
+//             path: 'user'
+//         }
+//     })
+//     .exec(function(err, posts){
+//         User.find({}, function(err, users){
+//             if(err){
+//                 console.log('cannot fetch users from the db');
+//                 return;
+//             }
+//             return res.render('home', {
+//                 title: 'Codial | Home',
+//                 posts: posts,
+//                 all_users: users 
+//             });
+//         });
+//     });
+// }
+
+// Using then
+// Post.find({}).populate('comments').then('callback function');
+
+// let posts = Post.find({}).populate('comments').exec();
+// posts.then();
+
+// Using async await
+
+module.exports.home = async function(req, res){
+    try{
+        let posts = await Post.find({}).populate('user').populate({
+            path: 'comments',
+            populate:{
+                path: 'user' 
             }
-            return res.render('home', {
-                title: 'Codial | Home',
-                posts: posts,
-                all_users: users 
-            });
         });
-    });
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codial | Home",
+            posts: posts,
+            all_users: users 
+        });
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
